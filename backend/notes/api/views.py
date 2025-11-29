680701ff4137232c3d9371fcfe3de6702ebda9a9
CoffeeAndCommit <medhavisharma11apr@gmail.com>
2025-11-29 16:27:52 +0530

    whole done

--- backend/notes/api/views.py
+++ backend/notes/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import django.http as http
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
 def getRoutes(request):
